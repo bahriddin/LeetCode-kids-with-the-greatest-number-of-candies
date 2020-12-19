3a08f90c1b7fbdaf1b91c5d067aa15cf48457d17
bahriddin <bahriddin.abdiev@ferocia.com.au>
2020-12-19 14:02:06 +1100

    Refactor the solution

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
+// Problem description: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
 /**
  * @param {number[]} candies
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-var kidsWithCandies = function(candies, extraCandies) {
-    let max = Math.max(...candies);
-    
-    function isTrue(arg){
-        if (arg + extraCandies < max)
-            return false;
-        else 
-            return true;
-    }
-    
-    return candies.map(isTrue);
-    
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = Math.max(...candies);
+
+  function isTrue(arg) {
+    return arg + extraCandies >= max;
+  }
+
+  return candies.map(isTrue);
 };
